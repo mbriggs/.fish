a250a47136b8f056ffaf63b7fb193d5f60912861
Matt Briggs <matt@mattbriggs.net>
2015-05-24 01:40:10 -0400

    sstuff

--- config.fish
+++ config.fish
@@ -2,4 +2,4 @@ source ./init.fish
 source ./globals.fish
 source ./plugins.fish
 source ./functions.fish
-source ./pure.fish
+source ./prompt.fish
--- init.fish
+++ init.fish
@@ -1,11 +1,3 @@
 if test -f $HOME/.ssh/id_rsa
   ssh-add $HOME/.ssh/id_rsa >/dev/null ^&1
 end
-
-if test uname = "Darwin"
-    alias ln="gln"
-    alias ls="gls --color"
-else
-    alias ls="ls --color"
-    set -U TERM xterm-256color
-end
